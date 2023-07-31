6c722428d2e164b00465c579a8def31d99214257
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2023-07-31 12:15:09 +0100

    Register signup route handler

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { signup } = require("../controller/authController");
+
+router.post("/signup", signup);
+
+module.exports = router;
