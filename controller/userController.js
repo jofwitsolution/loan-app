c5a7dc51dddcfcda1db20e10333e7e5e38dd910f
Oluwafemi Faleye <faleyeoluwafemi1@gmail.com>
2023-08-17 12:08:21 +0100

    Refactor code

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,13 @@
 const { User } = require("../models/User");
 const { Account } = require("../models/Account");
 const { LoanRequest } = require("../models/LoanRequest");
-const { validateLoanRequest } = require("../lib/validation/loanValidation");
 const { Transaction } = require("../models/Transaction");
+const { validateLoanRequest } = require("../lib/validation/loanValidation");
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../lib/error");
 
 const accountId = process.env.ACCOUNT_ID;
 
@@ -10,127 +15,91 @@ const accountId = process.env.ACCOUNT_ID;
 // @Desc: Get all users
 // @Access: private/admin
 const getUsers = async (req, res, next) => {
-  try {
-    const users = await User.find({}).select("-password -isAuthorizedAdmin");
+  const users = await User.find({}).select("-password -isAuthorizedAdmin");
 
-    res.json({ success: true, users });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ success: true, users });
 };
 
 // @Method: GET /users/:id/profile
 // @Desc: User profile
 // @Access: private
 const getProfile = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id).select(
-      "-password -isAuthorizedAdmin"
-    );
-    if (!user) {
-      res.status(404).json({ success: false, msg: "User not found" });
-      return;
-    }
-
-    res.json({ success: true, user });
-  } catch (error) {
-    next(error);
+  const user = await User.findById(req.params.id).select(
+    "-password -isAuthorizedAdmin"
+  );
+  if (!user) {
+    throw new NotFoundError("User not found");
   }
+
+  res.json({ success: true, user });
 };
 
 // @Method: POST /users/:userId/request-loan
 // @Desc: Make loan request
 // @Access: private
 const requestLoan = async (req, res, next) => {
-  try {
-    // validate body of request (amount)
-    const error = await validateLoanRequest(req.body);
-    if (error) {
-      res.status(400).json({ msg: error, success: false });
-      return;
-    }
-
-    if (req.params.userId !== req.user._id.toString()) {
-      res.status(403).json({
-        msg: `Invalid user.`,
-        success: false,
-      });
-      return;
-    }
-
-    const user = await User.findById(req.params.userId);
-    if (!user) {
-      res.status(404).json({
-        msg: `User not found.`,
-        success: false,
-      });
-      return;
-    }
-
-    // check if amount is not less than or greater than min/max request
-    const account = await Account.findById(accountId);
-    if (!account) {
-      res.status(404).json({ msg: "No account found", success: false });
-      return;
-    }
-
-    const { amount } = req.body;
-
-    if (amount < account.minLoan || amount > account.maxLoan) {
-      res.status(400).json({
-        msg: `Minimum request is ${account.minLoan} and maximum request is ${account.maxLoan}.`,
-        success: false,
-      });
-      return;
-    }
-
-    if (user.hasLoanRequest) {
-      res
-        .status(401)
-        .json({ success: false, msg: "You already have a loan request" });
-      return;
-    }
-
-    const loan = new LoanRequest({
-      user: user._id,
-      amount,
-    });
-
-    await loan.save();
-
-    user.hasLoanRequest = true;
-    await user.save();
-
-    res.status(201).json({ success: true, loanRequest: loan });
-  } catch (error) {
-    next(error);
+  // validate body of request (amount)
+  const error = await validateLoanRequest(req.body);
+  if (error) {
+    throw new BadRequestError(error);
   }
+
+  if (req.params.userId !== req.user._id.toString()) {
+    throw new UnauthorizedError("You are not authorized");
+  }
+
+  const user = await User.findById(req.params.userId);
+  if (!user) {
+    throw new NotFoundError("User not found");
+  }
+
+  // check if amount is not less than or greater than min/max request
+  const account = await Account.findById(accountId);
+  if (!account) {
+    throw new NotFoundError("No account found");
+  }
+
+  const { amount } = req.body;
+
+  if (amount < account.minLoan || amount > account.maxLoan) {
+    throw new BadRequestError(
+      `Minimum request is ${account.minLoan} and maximum request is ${account.maxLoan}.`
+    );
+  }
+
+  if (user.hasLoanRequest) {
+    throw new UnauthorizedError("You already have a loan request");
+  }
+
+  const loan = new LoanRequest({
+    user: user._id,
+    amount,
+  });
+
+  await loan.save();
+
+  user.hasLoanRequest = true;
+  await user.save();
+
+  res.status(201).json({ success: true, loanRequest: loan });
 };
 
 // @Method: GET /users/:userId/loan-requests
 // @Desc: Get user requests
 // @Access: private
 const getUserLoanRequests = async (req, res, next) => {
-  try {
-    const loanRequests = await LoanRequest.find({ user: req.params.userId });
+  const loanRequests = await LoanRequest.find({ user: req.params.userId });
 
-    res.json({ loanRequests });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ loanRequests });
 };
 
 // @Method: GET /users/:userId/transactions
 // @Desc: Get user transactions
 // @Access: private
 const getUserTransactions = async (req, res, next) => {
-  try {
-    const transactions = await Transaction.find({ user: req.params.userId });
+  const transactions = await Transaction.find({ user: req.params.userId });
 
-    res.json({ transactions });
-  } catch (error) {
-    next(error);
-  }
+  res.json({ transactions });
 };
 
 module.exports.getProfile = getProfile;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-// require("express-async-errors");
+require("express-async-errors");
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const { dbConnect } = require("./lib/dbConnect");
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 const authRoutes = require("./routes/auth");
@@ -12,7 +14,15 @@ const codeRoutes = require("./routes/code");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_PRIVATE_KEY));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -21,11 +31,18 @@ app.use("/api/loans", loanRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/codes", codeRoutes);
 
-app.use(errorHandler);
 app.use(notFound);
-
-dbConnect();
+app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Server listenning on port ${port}...`));
+async function start() {
+  try {
+    await dbConnect();
+    app.listen(port, () => console.log(`Server listenning on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
